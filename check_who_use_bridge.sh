81b9aa1a38308e3d91453b2cef941a25f3d76984
anascko <anascko@gmail.com>
2016-09-19 12:02:37 +0300

    Update check_who_use_bridge.sh
    If any interface don't use eth2 we can add
    And if ssh key not exist - not check it

--- check_who_use_bridge.sh
+++ check_who_use_bridge.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
+your_bridge=$1
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 VNET=`(brctl show | awk '/p4p2/ {print $1}')`
-VNET1=`(echo $VNET | cut -c1-4)`
-#declare -a name_vm
-name_vm=""
-if [[ $VNET1 == "fuel" ]]
+if [[ $VNET == "" ]]
     then
-      for vm in $(virsh list --name)
-        do 
-          name_vm=`virsh dumpxml $vm|grep -q "$VNET" && echo $vm`
+    stp_check=`(brctl show | grep "$your_bridge" | awk '{print $3}')`
+    echo $stp_check
+        if [[ $stp_check == "yes" ]]
+	then
+	    brctl stp $your_bridge no
+	fi
+    brctl addif $your_bridge p4p2
+    exit 1;
+fi
+# check if exist
+else
+    VNET1=`(echo $VNET | cut -c1-4)`
+    name_vm=""
+    if [[ $VNET1 == "fuel" ]]
+	then
+	for vm in $(virsh list --name)
+        do
+	    name_vm=`virsh dumpxml $vm|grep -q "$VNET" && echo $vm`
         done
-#VNET1=`(ifconfig $VNET | awk '/HWaddr/ {print $5}')`
-     else 
+
+    else
 #it's devsteck
      adr_node=`(ifconfig $VNET | awk '/addr:/ {print  $2}' |cut -c6-21 | cut -d. -f 1,2,3)`
 #     echo $adr_node
-     arp_exist=`(sudo arping -c 1 $adr_node.1 | head -2 | awk '/bytes/ {print $1}')`
+     ping_exist=`(ping -c 1 $adr_node.1 | head -2 | awk '/bytes from/ {print $1}')`
 #echo $mac
-    if [[ $arp_exist == '42' ]]
-      then
-        VNET_mac=`(sshpass -p r00tme ssh root@$adr_node.1 'ifconfig eth1 | grep HWaddr' | awk '{print $5}')`
-	mac_vm=""        	
-		for vm in $(virsh list --name)
-		  do
-            mac_vm=`virsh dumpxml $vm|grep "$VNET_mac"`
-              if [[ $mac_vm != "" ]]
-                 then 
-		echo "$vm"
-                name_vm=$vm
-	      fi 
+	if [[ $ping_exist == '64' ]]
+      	then
+    	    VNET_mac=`(sshpass -p r00tme ssh -o "StrictHostKeyChecking no" root@$adr_node.1 'ifconfig eth1 | grep HWaddr' | awk '{print $5}')`
+	    mac_vm=""        	
+	    for vm in $(virsh list --name)
+		do
+        	mac_vm=`virsh dumpxml $vm|grep "$VNET_mac"`
+              	    if [[ $mac_vm != "" ]]
+            		then 
+                	name_vm=$vm
+	      	    fi 
            	done
 #        echo $name_vm
-      else
-          exit 1
-    fi
+  	else
+    	    exit 1
+    	fi
+    
 fi
 # adr_node=`(ifconfig $VNET | awk '/addr:/ {print  $2}' |cut -c6-21)
-echo "Please ask's admin of $name_vm turn off bridge $VNET from p4p2"
+echo "Please ask's admin of $name_vm or to vsaienko@mirantis.com turn off bridge $VNET from p4p2"
