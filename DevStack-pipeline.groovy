efc0be02ee1aaa8350c0221b8c1038bc218912cf
anascko <anascko@gmail.com>
2017-08-07 16:27:17 +0300

    Update DevStack-pipeline.groovy

--- DevStack-pipeline.groovy
+++ DevStack-pipeline.groovy
@@ -8,30 +8,29 @@
 **/
  
 
-node() {
-    def LIBVIRT_SOCKET = "--connect=qemu:///system"
-    if (DISTRO_RELEASE == xenial){
-        old_img = 'devstack-generic-ubuntu-xenial'
-    }
-    else {
-        'Distro release not present'
-    }
-    def new_img = "${env.ENV_NAME}"
+node {
+  def LIBVIRT_SOCKET = "--connect=qemu:///system"
+  if (DISTRO_RELEASE == xenial){
+    old_img = 'devstack-generic-ubuntu-xenial'
+  }
+  else {
+    'Distro release not present'
+  }
+  def new_img = "${env.ENV_NAME}"
 
-    def SSHUSER = "cirros"
-    def SSHPASS = "cubswin:)"
-	
+  def SSHUSER = "cirros"
+  def SSHPASS = "cubswin:)"
+  try {
     stage ('Create VM') {
-        sh "set -xe && printenv"
-        sh "virt-clone ${LIBVIRT_SOCKET} -o ${old_img} -n ${new_img} --auto-clone || true"
-        sh "virsh start ${new_img} || true"
-        def mac = sh(script: "virsh domiflist ${new_img} | awk '/virbr0/ {print \$5}'", returnStdout: true)
-        println mac.text
-        def IP = sh(script: "/usr/sbin/arp -an  |grep ${mac} |awk '{print \$2}'", returnStdout: true)
-        IP = IP.replaceAll('\\)','')
-        def ENV_IP = IP.replaceAll('\\(','')
+      sh "set -xe && printenv"
+      sh "virt-clone ${LIBVIRT_SOCKET} -o ${old_img} -n ${new_img} --auto-clone || true"
+      sh "virsh start ${new_img} || true"
+      def mac = sh(script: "virsh domiflist ${new_img} | awk '/virbr0/ {print \$5}'", returnStdout: true)
+      println mac.text
+      def IP = sh(script: "/usr/sbin/arp -an  |grep ${mac} |awk '{print \$2}'", returnStdout: true)
+      IP = IP.replaceAll('\\)','')
+      def ENV_IP = IP.replaceAll('\\(','')
     }   
-
     
     stage ('Deploy Devstack') {
       writeFile file: '/tmp/ssh-config', text: """\
@@ -39,12 +38,12 @@ node() {
         UserKnownHostsFile /dev/null
         ForwardAgent yes
         Port 22
-    """.stripIndent()
+      """.stripIndent()
 
       writeFile file: '/tmp/local.conf', text: "${LOCAL_CONF}"
 
       writeFile file: '/tmp/sckrips.sh', text: """\
-        #!/bin/bash -x
+            #!/bin/bash -x
 	    set -e
 	    useradd -s /bin/bash -d /opt/stack -m stack
 	    sed -i "s/devstack-generic/$ENV_NAME/g" /etc/hosts
@@ -52,7 +51,7 @@ node() {
 	    echo "stack:cubswin:)" |  chpasswd
 	    git clone $DEVSTACK_REPO -b $DEVSTACK_BRANCH /opt/stack/devstack
 	    chown -R stack:stack /opt/stack/devstack
-      """.stripIndent()
+        """.stripIndent()
 
       sh "chmod +x /tmp/sckrips.sh"
 
@@ -63,5 +62,6 @@ node() {
         sh "sshpass -e scp -qF /tmp/ssh-config /tmp/local.conf stack@${ENV_IP}:/opt/stack/devstack"
         sh "sshpass -e ssh -F /tmp/ssh-config stack@${ENV_IP} cd devstack; ./stack.sh; exit"
       }
-   }
+    }
+  }
 }
